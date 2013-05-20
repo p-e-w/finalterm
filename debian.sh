6ba8619bb114960ef66668b10da5bd5be1a656ea
fuzzgun <bob@sluggish.dyndns.org>
2013-05-20 19:44:30 +0100

    Script to generate Debian package

---
+++ debian.sh
@@ -0,0 +1,28 @@
+# Run this script in order to generate a Debian package
+#
+# Ensure that the version number below is correct
+#
+# The directory within which the finalterm is contained
+# should have the name finalterm-x.xx, where the x's are
+# the version number.
+#
+# A corresponding version number entry must exist at the
+# top of debian/changelog and the email signature name
+# within that file should correspond exactly with your
+# GPG key.
+#
+#!/bin/bash
+
+APP=finalterm
+VERSION=0.10
+
+# clean
+rm -f ${APP} \#* \.#* debian/*.log debian/*.substvars debian/files
+rm -rf debian/deb.* debian/${APP} build obj-*
+rm -f ../${APP}*.deb ../${APP}*.changes ../${APP}*.asc ../${APP}*.dsc
+
+# Create a source archive
+tar -cvzf ../${APP}_${VERSION}.orig.tar.gz ../${APP}-${VERSION} --exclude=.git --exclude=build
+
+# Build the package
+fakeroot dpkg-buildpackage -F
